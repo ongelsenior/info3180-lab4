2b656468cacf6fe0f37a9a359cbc117ec39c8ee6
ongel senior <ongel.senior@mymona.uwi.edu>
2017-02-13 17:32:47 +0000

    started exercise 1

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,13 @@ from flask import Flask
 # Config Values
 USERNAME = 'admin'
 PASSWORD = 'password123'
+UPLOAD_FOLDER='./app/static/uploads'
 
 # SECRET_KEY is needed for session security, the flash() method in this case stores the message in a session
 SECRET_KEY = 'Sup3r$3cretkey'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+filefolder= app.config['UPLOAD_FOLDER']
+
 from app import views
